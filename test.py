9d95120351c4df7c176f4bc9de9ea7e39bf23d5c
Ubuntu <ubuntu@ip-172-31-7-15.ap-south-1.compute.internal>
2026-07-23 00:20:56 +0000

    added * and / also

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ print ("\nsimple python calculator program\n")
 num1 = 10
 num2 = 5
 
-print("Simple Python Calculator (+ and -)")
+print("\nSimple Python Calculator (+, -, *, /)\n)")
 print(f"Result: {num1} + {num2} = {num1 + num2}")
 print(f"Result: {num1} - {num2} = {num1 - num2}")
+print(f"Result: {num1} * {num2} = {num1 * num2}")
+print(f"Result: {num1} / {num2} = {num1 / num2}")
